0590432c3810350dff4afb61fa1a5574b34ca78c
Mita Saeko <mita@griffin-net.co.jp>
2020-06-12 16:22:20 +0900

    'canged'

--- www/js/script.js
+++ www/js/script.js
@@ -8,18 +8,25 @@ document.addEventListener('init', function (event) {
 
 
   // 最初の選択
-  // ＜色＞
   if (page.matches('#select-page')) {
 
     // 色の選択（color-select-pageへ）
     page.querySelector('#color-btn').onclick = function () {
       document.querySelector('#navigator').pushPage('color-select-page.html');
     };
+    // 気分の選択へ（mood-select-pageへ）
+    page.querySelector('#mood-btn').onclick = function () {
+      document.querySelector('#navigator').pushPage('mood-select-page.html');
+    };
+    // ジャンルの選択（genre-select-pageへ）
+    page.querySelector('#genre-btn').onclick = function () {
+      document.querySelector('#navigator').pushPage('genre-select-page.html');
+    };
+
     // トップボタン押したら戻る（color-select-pageから最初の選択画面へ）
   } else if (page.matches('#color-select-page')) {
 
     page.querySelector('#top-btn').onclick = function () {
-
       document.querySelector('#navigator').popPage();
     }
 
@@ -32,8 +39,8 @@ document.addEventListener('init', function (event) {
       };
     })
 
-    // トップボタン押したら戻る（number-color-pageから最初の選択画面へ）
   } else if (page.matches('#number-color-page')) {
+    // トップボタン押したら戻る（number-color-pageから最初の選択画面へ）
 
     page.querySelector('#top-btn').onclick = function () {
       document.querySelector('#navigator').popPage({ times: 2 });
@@ -44,34 +51,8 @@ document.addEventListener('init', function (event) {
         document.querySelector('#navigator').pushPage('recommend-page.html');
       };
     })
-    
-    // 作品タイトルと画像を表示
-    // （色の中でランダムに作品表示）
-  } else if (page.matches('#recommend-page')) { 
-    document.querySelector('#recommend-film-title')
-  
-    // トップボタン押したら戻る（recommend-pageから最初の選択画面へ）
-    page.querySelector('#top-btn').onclick = function () {
-      document.querySelector('#navigator').popPage({ times: 3 });
-
-      // とりあえず書いた (表示はまだ)
-      let infoElementArray = colorList.filter(element => element.color === state.color);
-      let infoElement = infoElementArray[Math.floor(Math.random() * infoElementArray.length)];
-    };
-  }
-
-
-
-
-  // <気分>
-  if (page.matches('#select-page')) {
-
-    // 気分の選択へ（mood-select-pageへ）
-    page.querySelector('#mood-btn').onclick = function () {
-      document.querySelector('#navigator').pushPage('mood-select-page.html');
-    };
-
-    // トップボタン押したら戻る（mood-select-pageから最初の選択画面へ）
+  ///////////////////////////////////////////
+  // トップボタン押したら戻る（mood-select-pageから最初の選択画面へ）
   } else if (page.matches('#mood-select-page')) {
     page.querySelector('#top-btn').onclick = function () {
       document.querySelector('#navigator').popPage();
@@ -97,37 +78,11 @@ document.addEventListener('init', function (event) {
         document.querySelector('#navigator').pushPage('recommend-page.html');
       };
     })
-    
-    // 作品タイトルと画像を表示
-    // 気分の中でランダムに作品表示
-  } else if (page.matches('#recommend-page')) {
 
-    // トップボタン押したら戻る（recommend-pageから最初の選択画面へ）
-    page.querySelector('#top-btn').onclick = function () {
-      document.querySelector('#navigator').popPage({ times: 3 });
-
-      // とりあえず書いた (表示はまだ)
-      let infoElementArray = moodList.filter(element => element.mood === state.mood);
-      let infoElement = infoElementArray[Math.floor(Math.random() * infoElementArray.length)];
-    };
-  }
-
-
-
-
-  // ＜ジャンル＞
-  if (page.matches('#select-page')) {
-
-    // ジャンルの選択（genre-select-pageへ）
-    page.querySelector('#genre-btn').onclick = function () {
-      document.querySelector('#navigator').pushPage('genre-select-page.html');
-    };
-
-    // トップボタン押したら戻る（genre-select-pageから最初の選択画面へ）
+  /////////////////////////// 
+  // トップボタン押したら戻る（genre-select-pageから最初の選択画面へ）
   } else if (page.matches('#genre-select-page')) {
-
     page.querySelector('#top-btn').onclick = function () {
-
       document.querySelector('#navigator').popPage();
     }
     // 数字の選択（genre-select-pageからnumber-genre-pageへ）
@@ -141,7 +96,6 @@ document.addEventListener('init', function (event) {
 
     // トップボタン押したら戻る（number-genre-pageから最初の選択画面へ）
   } else if (page.matches('#number-genre-page')) {
-
     page.querySelector('#top-btn').onclick = function () {
       document.querySelector('#navigator').popPage({ times: 2 });
     }
@@ -151,20 +105,34 @@ document.addEventListener('init', function (event) {
         document.querySelector('#navigator').pushPage('recommend-page.html');
       };
     })
-    
-    // 作品タイトルと画像を表示
-    // ジャンルの中でランダムに作品表示
-  } else if (page.matches('#recommend-page')) {
 
+    ////////////////////////////////
+    // 作品タイトルと画像を表示
+    // （色の中でランダムに作品表示）
+  } else if (page.matches('#recommend-page')) {
+    
     // トップボタン押したら戻る（recommend-pageから最初の選択画面へ）
     page.querySelector('#top-btn').onclick = function () {
       document.querySelector('#navigator').popPage({ times: 3 });
-
-      // とりあえず書いた（表示はまだ）
+    }
+    if (state.type === "color") {
+      let infoElementArray = colorList.filter(element => element.color === state.color);
+      let infoElement = infoElementArray[Math.floor(Math.random() * infoElementArray.length)];
+    } else if (state.type === "mood") {
+      let infoElementArray = moodList.filter(element => element.mood === state.mood);
+      let infoElement = infoElementArray[Math.floor(Math.random() * infoElementArray.length)];
+    } else if (state.type === "genre") {
       let infoElementArray = genreList.filter(element => element.genre === state.genre);
       let infoElement = infoElementArray[Math.floor(Math.random() * infoElementArray.length)];
     };
+
+    let recommend = document.querySelector("#recommend-film-title");
+    recommend.textContent = infoElement.title;
+    
+    // とりあえず書いた (表示はまだ)
   }
+
+
 
   // baldキャストがいた場合のみ表示したいページ
   // 写真もいれて
