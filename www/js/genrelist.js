5e818a7b2105a0582c49b404d7c984b9cc10946c
Mita Saeko <mita@griffin-net.co.jp>
2020-06-22 13:57:47 +0900

    'latest'

--- www/js/genrelist.js
+++ www/js/genrelist.js
@@ -117,13 +117,13 @@
     "title": "SPY",
     "genre": "1",
     "actor": "1",
-    "comment": "ハイテンションスパイ"
+    "comment": "こわいものなし"
   },
   {
     "title": "SPY",
     "genre": "15",
     "actor": "1",
-    "comment": "ハイテンションスパイ"
+    "comment": "こわいものなし"
   },
   {
     "title": "TIME",
@@ -813,7 +813,7 @@
     "title": "ダンサー、セルゲイ・ポルーニン 世界一優雅な野獣",
     "genre": "13",
     "actor": "26",
-    "comment": "Take me to churchを知る"
+    "comment": "Take me to church、いい曲"
   },
   {
     "title": "チャーリーズ・エンジェル",
@@ -1990,5 +1990,410 @@
     "genre": "16",
     "actor": "",
     "comment": "香港に行きたくなってしまう"
+  },
+  {
+    "title": "キングス・オブ・サマー",
+    "genre": "4",
+    "comment": "友情はどうする"
+  },
+  {
+    "title": "キングス・オブ・サマー",
+    "genre": "5",
+    "comment": "友情はどうする"
+  },
+  {
+    "title": "ブーリン家の姉妹",
+    "genre": "4",
+    "comment": "イギリスの歴史"
+  },
+  {
+    "title": "エスター",
+    "genre": "14",
+    "comment": "ありそうでこわい"
+  },
+  {
+    "title": "フォレスト・ガンプ／一期一会",
+    "genre": "4",
+    "comment": "フォレストの半生"
+  },
+  {
+    "title": "キャッチ・ミー・イフ・ユー・キャン",
+    "genre": "4",
+    "comment": "逃げる"
+  },
+  {
+    "title": "キャッチ・ミー・イフ・ユー・キャン",
+    "genre": "11",
+    "comment": "逃げる"
+  },
+  {
+    "title": "ジュラシック・ワールド",
+    "genre": "3",
+    "comment": "飼いたい"
+  },
+  {
+    "title": "へレディタリー／継承",
+    "genre": "14",
+    "comment": "痛い"
+  },
+  {
+    "title": "IT／イット”それ”が見えたら、終わり。",
+    "genre": "14",
+    "comment": "動きがはやい"
+  },
+  {
+    "title": "E.T.",
+    "genre": "2",
+    "comment": "しわしわの宇宙人"
+  },
+  {
+    "title": "E.T.",
+    "genre": "3",
+    "comment": "しわしわの宇宙人"
+  },
+  {
+    "title": "ヴィジット",
+    "genre": "14",
+    "comment": "気色悪くてこわい"
+  },
+  {
+    "title": "クリムゾン・ピーク",
+    "genre": "3",
+    "comment": "綺麗"
+  },
+  {
+    "title": "クリムゾン・ピーク",
+    "genre": "14",
+    "comment": "綺麗"
+  },
+  {
+    "title": "たかが世界の終わり",
+    "genre": "4",
+    "comment": "一家団欒ならず"
+  },
+  {
+    "title": "エレファント・ソング",
+    "genre": "4",
+    "comment": "どうしてそうなった"
+  },
+  {
+    "title": "早熟のアイオワ",
+    "genre": "4",
+    "comment": "過酷"
+  },
+  {
+    "title": "ジンジャーの朝～さよなら、わたしが愛した世界",
+    "genre": "4",
+    "comment": "嫌な気持ちになる"
+  },
+  {
+    "title": "ジンジャーの朝～さよなら、わたしが愛した世界",
+    "genre": "6",
+    "comment": "嫌な気持ちになる"
+  },
+  {
+    "title": "チョコレートドーナツ",
+    "genre": "4",
+    "comment": "救いを"
+  },
+  {
+    "title": "レ・ミゼラブル",
+    "genre": "4",
+    "comment": "壮大"
+  },
+  {
+    "title": "レ・ミゼラブル",
+    "genre": "7",
+    "comment": "壮大"
+  },
+  {
+    "title": "ぼくとアールと彼女のさよなら",
+    "genre": "4",
+    "comment": "友情"
+  },
+  {
+    "title": "ぼくとアールと彼女のさよなら",
+    "genre": "5",
+    "comment": "友情"
+  },
+  {
+    "title": "ぼくとアールと彼女のさよなら",
+    "genre": "6",
+    "comment": "友情"
+  },
+  {
+    "title": "グリーンブック",
+    "genre": "4",
+    "comment": "いい話"
+  },
+  {
+    "title": "グリーンブック",
+    "genre": "7",
+    "comment": "いい話"
+  },
+  {
+    "title": "パンズラビリス",
+    "genre": "3",
+    "comment": "一度迷い込みたい"
+  },
+  {
+    "title": "ゼロ・グラビティ",
+    "genre": "2",
+    "comment": "重力か無重力か"
+  },
+  {
+    "title": "ゼロ・グラビティ",
+    "genre": "4",
+    "comment": "重力か無重力か"
+  },
+  {
+    "title": "イノセントガーデン",
+    "genre": "4",
+    "comment": "血は争えないという話"
+  },
+  {
+    "title": "イノセントガーデン",
+    "genre": "10",
+    "comment": "血は争えないという話"
+  },
+  {
+    "title": "ロブスター",
+    "genre": "3",
+    "comment": "摩訶不思議"
+  },
+  {
+    "title": "ロブスター",
+    "genre": "4",
+    "comment": "摩訶不思議"
+  },
+  {
+    "title": "ハイ・ライズ",
+    "genre": "10",
+    "comment": "タワマンのヒエラルキー"
+  },
+  {
+    "title": "ネオン・デーモン",
+    "genre": "3",
+    "comment": "ぎょっとする"
+  },
+  {
+    "title": "ネオン・デーモン",
+    "genre": "10",
+    "comment": "ビジュアルに騙された"
+  },
+  {
+    "title": "エヴォリューション",
+    "genre": "9",
+    "comment": "ディストピア"
+  },
+  {
+    "title": "バック・トゥ・ザ・フューチャー",
+    "genre": "2",
+    "comment": "おもしろい"
+  },
+  {
+    "title": "バック・トゥ・ザ・フューチャー",
+    "genre": "4",
+    "comment": "見てよかった"
+  },
+  {
+    "title": "バック・トゥ・ザ・フューチャー",
+    "genre": "6",
+    "comment": "リックアンドモーティみたい"
+  },
+  {
+    "title": "マトリックス",
+    "genre": "1",
+    "comment": "柔軟性"
+  },
+  {
+    "title": "マトリックス",
+    "genre": "2",
+    "comment": "柔軟性"
+  },
+  {
+    "title": "ミスト",
+    "genre": "2",
+    "comment": "最悪"
+  },
+  {
+    "title": "ミスト",
+    "genre": "9",
+    "comment": "最悪"
+  },
+  {
+    "title": "ミスト",
+    "genre": "14",
+    "comment": "最悪"
+  },
+  {
+    "title": "スター・ウォーズ",
+    "genre": "2",
+    "comment": "アトラクションみたいで楽しい"
+  },
+  {
+    "title": "スター・ウォーズ",
+    "genre": "3",
+    "comment": "ジャージャービンクスがいいですよ"
+  },
+  {
+    "title": "イエスタデイ",
+    "genre": "3",
+    "comment": "ビートルズの曲を知る"
+  },
+  {
+    "title": "イエスタデイ",
+    "genre": "4",
+    "comment": "ビートルズの曲を知る"
+  },
+  {
+    "title": "イエスタデイ",
+    "genre": "7",
+    "comment": "ビートルズの曲がいっぱい"
+  },
+  {
+    "title": "ゴーストバスターズ",
+    "genre": "3",
+    "comment": "わくわく"
+  },
+  {
+    "title": "ゴーストバスターズ",
+    "genre": "14",
+    "comment": "わくわく"
+  },
+  {
+    "title": "ゴーストバスターズ（2016）",
+    "genre": "3",
+    "comment": "マシュマロマン"
+  },
+  {
+    "title": "ゴーストバスターズ（2016）",
+    "genre": "14",
+    "comment": "マシュマロマン"
+  },
+  {
+    "title": "ピッチ・パーフェクト",
+    "genre": "6",
+    "comment": "アカペラサークル"
+  },
+  {
+    "title": "ピッチ・パーフェクト",
+    "genre": "7",
+    "comment": "アカペラサークル"
+  },
+  {
+    "title": "マンマ・ミーア！",
+    "genre": "7",
+    "comment": "歌って踊る"
+  },
+  {
+    "title": "マンマ・ミーア！",
+    "genre": "16",
+    "comment": "みんなABBA歌う"
+  },
+  {
+    "title": "ロケットマン",
+    "genre": "4",
+    "comment": "幼少期そっくり"
+  },
+  {
+    "title": "ロケットマン",
+    "genre": "7",
+    "comment": "衣装がド派手"
+  },
+  {
+    "title": "SING／シング",
+    "genre": "7",
+    "comment": "かわいい"
+  },
+  {
+    "title": "SING／シング",
+    "genre": "8",
+    "comment": "キャラが立ってる"
+  },
+  {
+    "title": "シュレック",
+    "genre": "8",
+    "comment": "字幕、吹替えどちらもいい"
+  },
+  {
+    "title": "シュレック",
+    "genre": "12",
+    "comment": "字幕、吹替えどちらもいい"
+  },
+  {
+    "title": "ルーム",
+    "genre": "4",
+    "comment": "ひりひりする"
+  },
+  {
+    "title": "ルーム",
+    "genre": "10",
+    "comment": "ひりひりする"
+  },
+  {
+    "title": "トム・アット・ザ・ファーム",
+    "genre": "4",
+    "comment": "不穏"
+  },
+  {
+    "title": "トム・アット・ザ・ファーム",
+    "genre": "10",
+    "comment": "不穏"
+  },
+  {
+    "title": "スーパー・サイズ・ミー",
+    "genre": "13",
+    "comment": "危険"
+  },
+  {
+    "title": "もしも建物が話せたら",
+    "genre": "13",
+    "comment": "夢"
+  },
+  {
+    "title": "ゲットスマート",
+    "genre": "1",
+    "comment": "現場に出たい！"
+  },
+  {
+    "title": "ゲットスマート",
+    "genre": "12",
+    "comment": "これでもスパイ"
+  },
+  {
+    "title": "ゲットスマート",
+    "genre": "15",
+    "comment": "これでもスパイ"
+  },
+  {
+    "title": "エージェント・ウルトラ",
+    "genre": "4",
+    "comment": "CIAだったとは"
+  },
+  {
+    "title": "エージェント・ウルトラ",
+    "genre": "15",
+    "comment": "CIAだったとは"
+  },
+  {
+    "title": "ナイト＆デイ",
+    "genre": "1",
+    "comment": "なんでもあり"
+  },
+  {
+    "title": "ナイト＆デイ",
+    "genre": "15",
+    "comment": "なんでもあり"
+  },
+  {
+    "title": "インファナル・アフェア",
+    "genre": "11",
+    "comment": "苦しい"
+  },
+  {
+    "title": "インファナル・アフェア",
+    "genre": "15",
+    "comment": "苦しい"
   }
 ]
